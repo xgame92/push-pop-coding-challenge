9ec7f0fe05564b7c74a5c9fd22cfb1c4156bc42c
Yiğit Tanrıverdi <x_game92@hotmail.com>
2021-02-21 19:59:27 +0100

    add nice overview for error page

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,16 +1,63 @@
 <template>
-  <div>
-    <div>
-      Resolved:
-      <div v-for="error in resolved" :key="error.index">`{{ error.code }}` - {{ error.text }}</div>
-    </div>
-    <div>
-      Unresolved:
-      <div v-for="error in unresolved" :key="error.index">`{{ error.code }}` - {{ error.text }}</div>
-    </div>
-    <div>
-      Backlog:
-      <div v-for="error in backlog" :key="error.index">`{{ error.code }}` - {{ error.text }}</div>
+  <div class="container">
+    <div class="row justify-content-md-center pt-4">
+      <div class="col-12">
+        <div class="card">
+          <div class="card-header mb-2 text-center bg-white">
+            <p class="h1">Error List</p>
+          </div>
+          <div class="row mb-3 p-2">
+            <div class="col-4">
+              <div class="card">
+                <div class="card-header mb-2 text-center bg-warning">
+                  <p class="text-white">Unresolved</p>
+                </div>
+                <div class="row">
+                  <div class="col-12">
+                    <div class="error__container unresolved__bg" v-for="error in unresolved" :key="error.index">
+
+                      <div class="error__text">{{ error.text }}</div>
+                      <div class="error__code bg-warning text-white">{{ error.code }}</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-4">
+              <div class="card">
+                <div class="card-header mb-2 text-center bg-success">
+                  <p class="text-white">Resolved</p>
+                </div>
+                <div class="row">
+                    <div class="col-12">
+                      <div class="error__container resolved__bg" v-for="error in resolved" :key="error.index">
+
+                        <div class="error__text">{{ error.text }}</div>
+                        <div class="error__code bg-success text-white">{{ error.code }}</div>
+                      </div>
+                    </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-4">
+              <div class="card">
+                <div class="card-header mb-2 text-center bg-info">
+                  <p class="text-white">Backlog</p>
+                </div>
+                <div class="row">
+                  <div class="col-12">
+                    <div class="error__container backlog__bg" v-for="error in backlog" :key="error.index">
+
+                      <div class="error__text">{{ error.text }}</div>
+                      <div class="error__code bg-info text-white">{{ error.code }}</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -45,3 +92,33 @@ export default {
   }
 };
 </script>
+<style>
+.unresolved__bg{
+  background-color: #fff3cd;
+}
+
+.resolved__bg{
+ background-color: #d1e7dd;
+}
+
+.backlog__bg{
+background-color: #cff4fc;
+}
+.error__container{
+  display: flex;
+  padding: 5px 10px;
+  margin: 5px 10px;
+  border-radius: 5px;
+}
+.error__code{
+  border-radius: 5px;
+  max-height: 25px;
+  font-weight: 700;
+  padding: 0 7px;
+  align-self: center;
+}
+.error__text{
+  font-size: 16px;
+  margin: 5px 10px;
+}
+</style>
