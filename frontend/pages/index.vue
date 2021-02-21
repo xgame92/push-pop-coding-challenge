7bba21236688c9d21cd164f5e23be6387e54e430
Yiğit Tanrıverdi <x_game92@hotmail.com>
2021-02-21 21:26:11 +0100

    - add undo button and functionality for last action

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -3,10 +3,20 @@
     <div class="row justify-content-md-center pt-4">
       <div class="col-12">
         <div class="card">
-          <div class="card-header mb-2 text-center bg-white">
+          <div class="card-header text-center bg-white">
             <p class="h1">Error List</p>
           </div>
           <div class="row mb-3 p-2">
+            <div v-if="cachedAction.length > 0" class="col-12">
+              <div class="error__undo-btn"></div>
+              <button
+                type="button"
+                class="btn bg-danger btn-lg text-white mb-2"
+                @click="onClickUndo()"
+              >
+                Undo
+              </button>
+            </div>
             <div class="col-4">
               <div class="card">
                 <div class="card-header mb-2 text-center bg-warning">
@@ -23,7 +33,8 @@
                           type="button"
                           class="btn btn-success btn-sm"
                           @click="onClickResolve(error)"
-                        >Resolve</button>
+                        >Resolve
+                        </button>
                       </div>
                     </div>
                   </div>
@@ -36,20 +47,21 @@
                   <p class="text-white">Resolved</p>
                 </div>
                 <div class="row">
-                    <div class="col-12">
-                      <div class="error__container resolved__bg" v-for="error in resolved" :key="error.index">
+                  <div class="col-12">
+                    <div class="error__container resolved__bg" v-for="error in resolved" :key="error.index">
 
-                        <div class="error__text">{{ error.text }}</div>
-                        <div class="error__code bg-success text-white">{{ error.code }}</div>
-                        <div class="error__btn">
-                          <button
-                            type="button"
-                            class="btn bg-warning btn-sm text-white"
-                            @click="onClickUnresolve(error)"
-                          >Unresolve</button>
-                        </div>
+                      <div class="error__text">{{ error.text }}</div>
+                      <div class="error__code bg-success text-white">{{ error.code }}</div>
+                      <div class="error__btn">
+                        <button
+                          type="button"
+                          class="btn bg-warning btn-sm text-white"
+                          @click="onClickUnresolve(error)"
+                        >Unresolve
+                        </button>
                       </div>
                     </div>
+                  </div>
                 </div>
               </div>
             </div>
@@ -69,7 +81,8 @@
                           type="button"
                           class="btn bg-warning btn-sm text-white"
                           @click="onClickUnresolveFromBacklog(error)"
-                        >Unresolve</button>
+                        >Unresolve
+                        </button>
                       </div>
                     </div>
                   </div>
@@ -85,9 +98,9 @@
 
 <script>
 export default {
-  async asyncData({ $axios }) {
+  async asyncData({$axios}) {
     try {
-      const { resolved, unresolved, backlog } = await $axios.$get(
+      const {resolved, unresolved, backlog} = await $axios.$get(
         "http://localhost:8000/get_lists"
       );
       return {
@@ -108,11 +121,24 @@ export default {
     return {
       resolved: [],
       unresolved: [],
-      backlog: []
+      backlog: [],
+      cachedAction: []
     };
   },
   methods: {
-    onClickResolve(selectedError){
+    onClickResolve(selectedError) {
+
+      this.cachedAction.push(
+        {
+          propName: 'unresolved',
+          data: this.unresolved
+        },
+        {
+          propName: 'resolved',
+          data: this.resolved
+        }
+      );
+
       const error = this.unresolved.find(err => err.index === selectedError.index);
 
       this.unresolved = this.unresolved.filter(err => err.index !== error.index)
@@ -121,7 +147,19 @@ export default {
 
       //TODO Make a call for RESOLVE
     },
-    onClickUnresolve(selectedError){
+    onClickUnresolve(selectedError) {
+
+      this.cachedAction.push(
+        {
+          propName: 'unresolved',
+          data: this.unresolved
+        },
+        {
+          propName: 'resolved',
+          data: this.resolved
+        }
+      );
+
       const error = this.resolved.find(err => err.index === selectedError.index);
 
       this.resolved = this.resolved.filter(err => err.index !== error.index)
@@ -130,47 +168,79 @@ export default {
 
       //TODO Make a call for UNRESOLVE
     },
-    onClickUnresolveFromBacklog(selectedError){
+    onClickUnresolveFromBacklog(selectedError) {
+
+      this.cachedAction.push(
+        {
+          propName: 'backlog',
+          data: this.backlog
+        },
+        {
+          propName: 'unresolved',
+          data: this.unresolved
+        }
+      );
+
       const error = this.backlog.find(err => err.index === selectedError.index);
 
       this.backlog = this.backlog.filter(err => err.index !== error.index)
 
       this.unresolved = [...this.unresolved, error]
+    },
+    onClickUndo() {
+      this.cachedAction.forEach(action => {
+        this.[action.propName] = action.data;
+      })
+
+      this.cachedAction = [];
+
+      //TODO Make a call for UNDO
     }
   }
 };
 </script>
 <style>
-.unresolved__bg{
+.unresolved__bg {
   background-color: #fff3cd;
 }
 
-.resolved__bg{
- background-color: #d1e7dd;
+.resolved__bg {
+  background-color: #d1e7dd;
 }
 
-.backlog__bg{
-background-color: #cff4fc;
+.backlog__bg {
+  background-color: #cff4fc;
 }
-.error__container{
+
+.error__container {
   display: flex;
   padding: 5px 10px;
   margin: 5px 10px;
   border-radius: 5px;
 }
-.error__code{
+
+.error__code {
   border-radius: 5px;
   max-height: 25px;
   font-weight: 700;
   padding: 0 7px;
   align-self: center;
 }
-.error__text{
+
+.error__text {
   font-size: 16px;
   margin: 5px 10px;
 }
-.error__btn{
+
+.error__btn {
   align-self: center;
   margin: 5px;
+}
+
+.error__undo-btn {
+  display: flex;
+  flex-direction: column;
+  padding: 0;
+  margin: 0;
 }
 </style>
